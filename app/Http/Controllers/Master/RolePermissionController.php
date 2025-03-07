e1e750a5d6ff333b62642c4201128316deb7925e
casperdias <dias287804@gmail.com>
2025-03-07 13:56:23 +0700

    Update Role Permission Setting

--- app/Http/Controllers/Master/RolePermissionController.php
+++ app/Http/Controllers/Master/RolePermissionController.php
@@ -5,7 +5,50 @@ namespace App\Http\Controllers\Master;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\Role;
+use App\Models\Permission;
+
+use Inertia\Inertia;
+use Inertia\Response;
+
 class RolePermissionController extends Controller
 {
-    //
+    public function index(Request $request, Role $role)
+    {
+        $search = $request->input('search');
+
+        $permissions = Permission::where('name', 'like', "%{$search}%")
+            ->orWhere('display_name', 'like', "%{$search}%")
+            ->paginate(5);
+
+        $permissions->getCollection()->transform(function ($permission) use ($role) {
+            $permission->status = $role->hasPermissionTo($permission->name);
+            return $permission;
+        });
+
+        return Inertia::render('master/RolePermission', [
+            'role' => $role,
+            'permissions' => $permissions,
+            'search' => $search,
+        ]);
+    }
+
+    public function store(Request $request, Role $role, Permission $permission)
+    {
+        $request->validate([
+            'status' => 'required|boolean',
+        ]);
+
+        if ($request->status) {
+            $role->givePermission($permission);
+        } else {
+            $role->revokePermission($permission);
+        }
+
+        return to_route('role.setting', [
+            'role' => $role->id,
+            'page' => $request->input('page', 1),
+            'search' => $request->input('search', ''),
+        ]);
+    }
 }
--- resources/js/pages/master/RolePermission.vue
+++ resources/js/pages/master/RolePermission.vue
@@ -1,7 +1,32 @@
 <script setup lang="ts">
+import { ref, watch } from "vue";
+import { router } from '@inertiajs/vue3';
 import AppLayout from '@/layouts/AppLayout.vue';
-import { type BreadcrumbItem } from '@/types';
+import { PaginationData, type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/vue3';
+import { Search } from 'lucide-vue-next';
+import { Input } from '@/components/ui/input';
+import TablePagination from '@/components/TablePagination.vue';
+import { Switch } from '@/components/ui/switch';
+
+const props = defineProps<{
+    role: Role;
+    permissions: PaginationData<Permission>;
+}>();
+
+interface Role {
+    id: number;
+    name: string;
+    display_name: string;
+}
+
+interface Permission {
+    id: number;
+    name: string;
+    display_name: string;
+    description: string;
+    status: boolean;
+}
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -10,13 +35,50 @@ const breadcrumbs: BreadcrumbItem[] = [
     },
     {
         title: 'Role',
-        href: '/master-data/role',
+        href: '/master-data/roles',
     },
     {
-        title: 'Role Permission Setting',
-        href: ''
-    }
+        title: `${props.role.display_name}`,
+        href: `/master-data/roles/${props.role.id}/setting`,
+    },
 ]
+
+const searchTerm = ref(route().params.search || "");
+let searchTimeout: ReturnType<typeof setTimeout>;
+
+watch(searchTerm, (newTerm) => {
+    clearTimeout(searchTimeout);
+    searchTimeout = setTimeout(() => {
+        router.get(props.permissions.path, {
+            search: newTerm,
+        }, {
+            preserveState: true,
+            preserveScroll: true,
+        });
+    }, 500);
+});
+
+const setupPermissionWatchers = () => {
+    props.permissions.data.forEach((permission) => {
+        watch(() => permission.status, (newStatus) => {
+            router.post(`${props.permissions.path}/${permission.id}`, {
+                status: newStatus,
+                page: props.permissions.current_page,
+                search: searchTerm.value,
+            }, {
+                preserveState: true,
+                preserveScroll: true,
+            });
+        });
+    });
+};
+
+setupPermissionWatchers();
+
+watch(() => props.permissions.data, () => {
+    setupPermissionWatchers();
+}, { deep: true });
+
 </script>
 
 <template>
@@ -24,6 +86,28 @@ const breadcrumbs: BreadcrumbItem[] = [
 
     <AppLayout :breadcrumbs="breadcrumbs">
         <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
+            <div class="relative w-full max-w-sm items-center">
+                <Input id="search" type="text" name="search" placeholder="Search..." class="pl-10" v-model="searchTerm" />
+                <span class="absolute start-0 inset-y-0 flex items-center justify-center px-2">
+                    <Search class="size-6 text-muted-foreground" />
+                </span>
+            </div>
+            <TablePagination
+                :columns="[
+                    { key: 'name', label: 'Name' },
+                    { key: 'display_name', label: 'Display Name' },
+                    { key: 'description', label: 'Description' },
+                ]"
+                :data="permissions.data"
+                :pagination="permissions"
+                :caption="'Pengaturan Permission Role'"
+                :actions="true"
+                :routeName="permissions.path"
+            >
+                <template v-slot:actions="{ item }">
+                    <Switch v-model="item.status" />
+                </template>
+            </TablePagination>
         </div>
     </AppLayout>
 </template>
