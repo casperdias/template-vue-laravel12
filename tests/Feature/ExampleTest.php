f04c347ffbce0f12e83058f0022a345d337e65e1
casperdias <dias287804@gmail.com>
2025-03-12 09:17:45 +0700

    Remove Unused Test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,7 +1,7 @@
 <?php
 
-it('returns a successful response', function () {
+it('returns to login', function () {
     $response = $this->get('/');
 
-    $response->assertStatus(200);
+    $response->assertRedirect('/login');
 });
